5bfc94fc368640bc73d14779a58b8239809b3967
ji kaiyang <jky.yang@gmail.com>
2015-09-18 13:32:42 +0900

    some modification

--- whatifarticle/tolatex.py
+++ whatifarticle/tolatex.py
@@ -41,26 +41,25 @@ class ToLatex(HTMLParser):
                 if d['id'] == 'question':
                     self.text.append('\\question')
                 elif d['id'] == 'attribute':
-                    self.text.append('\\hfill')
                     self.text.append('\\attribute')
             self.text.append('{')
         if tag == 'br':
             self.text.append('\n')
         if tag == 'em':
-            self.text.append(' \\emph{')
+            self.text.append('\\emph{')
         if tag == 'h1':
             if 'href' in self.text[-1]:
                 self.text[-1] = '{'
             self.text.append('\n\\chapter{')
         if tag == 'img':
             src = d['src'][1:] if not d['src'].startswith('http://what-if.xkcd.com/') else d['src'][len('http://what-if.xkcd.com/'):]
-            self.text.append(u'\n\\begin{{figure}}\n\\centering\n\\includegraphics[scale=0.8, max width=\\textwidth]{{{1}}}\n\\caption{{{0}}}\n\\end{{figure}}\n'.format(replacemark(d['title']), src))
+            self.text.append(u'\n\\begin{{figure}}\n\\centering\n\\includegraphics[scale=0.5, max width=\\textwidth]{{{1}}}\n\\caption{{{0}}}\n\\end{{figure}}\n'.format(replacemark(d['title']), src))
         if tag == 'a':
             link = replacemark(d['href'])
-            self.text.append(u' \\href{' + link + '}{')
+            self.text.append(u'\\href{' + link + '}{')
         if tag == 'span':
             if 'class' in d.keys() and d['class'] == 'refbody':
-                self.text.append(' \\footnote{')
+                self.text.append('\\footnote{')
             elif 'class' in d.keys() and d['class'] == 'refnum':
                 self.text.append('__del__')
             else:
@@ -70,7 +69,7 @@ class ToLatex(HTMLParser):
         if tag == 'img':
             d = dict(attrs)
             src = d['src'][1:] if not d['src'].startswith('http://what-if.xkcd.com/') else d['src'][len('http://what-if.xkcd.com/'):]
-            self.text.append(u'\n\\begin{{figure}}\n\\centering\n\\includegraphics[max width=\\textwidth]{{{1}}}\n\\caption{{{0}}}\n\\end{{figure}}\n'.format(replacemark(d['title']), src))
+            self.text.append(u'\n\\begin{{figure}}\n\\centering\n\\includegraphics[scale=0.5, max width=\\textwidth]{{{1}}}\n\\caption{{{0}}}\n\\end{{figure}}\n'.format(replacemark(d['title']), src))
         
     def handle_endtag(self, tag):
         if tag == 'h1':
@@ -81,7 +80,7 @@ class ToLatex(HTMLParser):
             if '__del__' in self.text:
                 self.text = self.text[:self.text.index('__del__')]
             else:
-                self.text.append('} ')
+                self.text.append('}')
         if tag == 'a':
             if self.text[-1].endswith('{'):
                 self.text[-1] = ""
